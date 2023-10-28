dfe8574da817f2384510c71b281a6faf065e0af0
gabi <gabriel.s.racz@gmail.com>
2023-10-27 21:34:24 -0400

    text improvements and fps counter example using callback

--- include/game/text.h
+++ include/game/text.h
@@ -1,6 +1,7 @@
 #ifndef TEXT_H
 #define TEXT_H
 
+#include <functional>
 #include "scene_node.h"
 
 class Game;
@@ -12,19 +13,30 @@ public:
         BOTTOMLEFT,
         TOPRIGHT,
         BOTTOMRIGHT,
-        CENTER
+        CENTER,
+        TOPCENTER,
+        BOTTOMCENTER
     };
 
     Text(const std::string name, Mesh* Mesh, Shader* shader, Game* game, const std::string& content)
     : SceneNode(name, Mesh, shader), content(content), game(game) {}
 
+    virtual void Update(double dt) override;
+
     Anchor anchor = Anchor::TOPLEFT;
+    void SetCallback(std::function<std::string(void)> f) {update_callback = f;}
+    void SetColor(const glm::vec4& newcol) {color = newcol;}
+    void SetBackgroundColor(const glm::vec4& newcol) {background_color = newcol;}
+    void SetAnchor(Anchor a) {anchor = a;}
+    void SetSize(float newsize) {size = newsize;}
 protected:
     virtual void SetUniforms(Shader* shader, Camera *camera, const glm::mat4 &parent_matrix) override;
 
-    std::string content;
-    glm::vec4 color;
-    glm::vec4 background_color;
+    float size = 13.125;
+    std::string content = "";
+    glm::vec4 color = {1.0f, 1.0f, 1.0f, 1.0f};
+    glm::vec4 background_color = {0.0f, 0.0f, 0.0f, 0.5f};
+    std::function<std::string(void)> update_callback;
     Game* game;
 };
 
