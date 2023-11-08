e249b86a3152883ed1d63a089a165fb4e9ea1ccc
Sean Meaney <seanmeaney@protonmail.com>
2023-11-08 00:26:20 -0500

    Just formated collision data and removed the zeroVec return

--- include/engine/collision_data.h
+++ include/engine/collision_data.h
@@ -10,43 +10,43 @@ enum CollisionType {
 };
 
 class CollisionData {
-public:
-    CollisionData() 
+   public:
+    CollisionData()
         : CollisionData(5.0) {}
 
-    CollisionData(float radius) 
+    CollisionData(float radius)
         : type(CollisionType::SPHERE), sphereRadius(radius), collisionEnabled(true) {}
 
-    CollisionData(const glm::vec3& halfSizes) 
+    CollisionData(const glm::vec3& halfSizes)
         : type(CollisionType::BOX), boxHalfSizes(halfSizes), collisionEnabled(true) {}
 
-    CollisionData(const glm::vec3& origin, const glm::vec3& direction) 
+    CollisionData(const glm::vec3& origin, const glm::vec3& direction)
         : type(CollisionType::RAY), rayOrigin(origin), rayDirection(glm::normalize(direction)), collisionEnabled(true) {}
 
     CollisionType GetType() const { return type; }
-    float GetSphereRadius() const { return collisionEnabled ? sphereRadius : 0.0f; }
-    const glm::vec3& GetBoxHalfSizes() const { static const glm::vec3 zeroVec(0.0f); return collisionEnabled ? boxHalfSizes : zeroVec; }
-    const glm::vec3& GetRayOrigin() const { static const glm::vec3 zeroVec(0.0f); return collisionEnabled ? rayOrigin : zeroVec; }
-    const glm::vec3& GetRayDirection() const { static const glm::vec3 zeroVec(0.0f); return collisionEnabled ? rayDirection : zeroVec; }
+    float GetSphereRadius() const { return sphereRadius; }
+    const glm::vec3& GetBoxHalfSizes() const { return boxHalfSizes; }
+    const glm::vec3& GetRayOrigin() const { return rayOrigin; }
+    const glm::vec3& GetRayDirection() const { return rayDirection; }
 
-    void SetSphereRadius(float radius)                  { sphereRadius = radius; }
-    void SetBoxHalfSizes(const glm::vec3& halfSizes)    { boxHalfSizes = halfSizes; }
-    void SetRayOrigin(const glm::vec3& origin)          { rayOrigin = origin; }
-    void SetRayDirection(const glm::vec3& direction)    { rayDirection = glm::normalize(direction); }
+    void SetSphereRadius(float radius) { sphereRadius = radius; }
+    void SetBoxHalfSizes(const glm::vec3& halfSizes) { boxHalfSizes = halfSizes; }
+    void SetRayOrigin(const glm::vec3& origin) { rayOrigin = origin; }
+    void SetRayDirection(const glm::vec3& direction) { rayDirection = glm::normalize(direction); }
 
     bool IsCollisionEnabled() const { return collisionEnabled; }
     void SetCollisionEnabled(bool enabled) { collisionEnabled = enabled; }
 
-private:
+   private:
     CollisionType type;
     bool collisionEnabled;
     // Sphere properties
-    float sphereRadius; // Only radius is needed, position will come from SceneNode's transform
+    float sphereRadius;  // Only radius is needed, position will come from SceneNode's transform
     // Box properties
-    glm::vec3 boxHalfSizes; // Half sizes of the box, position will come from SceneNode's transform
+    glm::vec3 boxHalfSizes;  // Half sizes of the box, position will come from SceneNode's transform
     // Ray properties
     glm::vec3 rayOrigin;
-    glm::vec3 rayDirection; 
+    glm::vec3 rayDirection;
 };
 
-#endif // __DEEP_NATURE_ALLIANCE_INCLUDE_ENGINE_COLLISION_DATA_H_
+#endif  // __DEEP_NATURE_ALLIANCE_INCLUDE_ENGINE_COLLISION_DATA_H_
