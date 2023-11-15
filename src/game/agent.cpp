0e6ab9f686cd53c5fb001edc994d710bce43695f
q <diskofgrain.xyz>
2023-11-14 19:10:21 -0500

    added fps code

---
+++ src/game/agent.cpp
@@ -0,0 +1,189 @@
+#define GLM_FORCE_RADIANS
+#include "agent.h"
+
+Agent::Agent(const std::string name, const std::string &mesh_id, const std::string shader_id, const std::string &texture_id) : SceneNode(name, mesh_id, shader_id, texture_id)
+{
+    vertical_velocity_ = 0.0;
+    vertical_offset_ = 0.0;
+    gravity_ = 9.8 * 3;
+    fall_speed_ = 55.0;
+    jump_speed_ = 30.0;
+    base_jump_speed_ = jump_speed_;
+    up_ = glm::vec3(0.0f, 1.0f, 0.0f);
+    jump_axis_ = glm::vec3(0.0, 1.0, 0.0f);
+    walk_direction_ = glm::vec3(0.0f, 0.0f, 0.0f);
+    step_height_ = 0.0f;
+    movement_damping_ = 0.0f;
+    step_offset_ = 0.0f;
+    speed_ = 30.0f;
+    sensitivity_ = -0.1f;
+    on_ground_ = false;
+    jumping_ = false;
+
+    forward_ = glm::vec3(0.0f);
+    backward_ = glm::vec3(0.0f);
+    strafe_left_ = glm::vec3(0.0f);
+    strafe_right_ = glm::vec3(0.0f);
+
+    target_position_ = glm::vec3(0.0f);
+    prev_position_ = glm::vec3(0.0f);
+}
+
+void Agent::UpMove()
+{
+    float step_height = 0.0f;
+    if (vertical_velocity_ < 0.0f)
+    {
+        step_height = step_height_;
+    }
+    target_position_ = transform.GetPosition() + up_ * (step_height_) + jump_axis_ * ((vertical_offset_ > 0.f ? vertical_offset_ : 0.f));
+
+    // CHECK FOR VERTICAL COLLISION
+    // VERTICAL COLLISION RESPONSE
+    transform.SetPosition(target_position_);
+    step_offset_ = step_height;
+}
+
+void Agent::WalkingMove(const glm::vec3 move)
+{
+    target_position_ = target_position_ + move;
+
+    // COLLISION SWEEPING AND CHECKING HERE FOR FORWARD SIDE TO SIDE OBJECTS
+
+    transform.SetPosition(target_position_);
+}
+
+void Agent::DownMove(float dt)
+{
+
+    if (vertical_velocity_ > 0.0f)
+    {
+        return;
+    }
+
+    glm::vec3 original_position_ = target_position_;
+
+    float down_velocity = (vertical_velocity_ < 0.0f ? -vertical_velocity_ : 0.0f) * dt;
+
+    if (down_velocity > 0.0 && down_velocity > fall_speed_ && (on_ground_ || !jumping_))
+    {
+        down_velocity = fall_speed_;
+    }
+
+    glm::vec3 drop_amount = up_ * (step_offset_ + down_velocity);
+    target_position_ -= drop_amount;
+
+    glm::vec3 position = transform.GetPosition();
+
+    if (target_position_.y <= -30.0)
+    { // CHECK FOR BELOW COLLISION
+
+        // Interpolate small fall (would usually want to do this before hitting floor)
+        float fraction = (position.y - (-30.0)) * 0.5;
+        transform.SetPosition(glm::mix(position, target_position_, fraction));
+
+        // On the ground yo
+        vertical_velocity_ = 0.0f;
+        vertical_offset_ = 0.0f;
+        jumping_ = false;
+    }
+    else
+    { // NO COLLISION i.e FELL FULL HEIGHT
+        transform.SetPosition(target_position_);
+    }
+}
+
+void Agent::Update(float dt)
+{
+    prev_position_ = transform.GetPosition();
+
+    on_ground_ = OnGround();
+
+    if (glm::length(walk_direction_) > 0)
+    {
+        walk_direction_ *= glm::pow(1.0f - movement_damping_, dt);
+    }
+
+    vertical_velocity_ *= glm::pow(1.0f - movement_damping_, dt);
+
+    vertical_velocity_ -= gravity_ * dt;
+    if (vertical_velocity_ > 0.0 && vertical_velocity_ > jump_speed_)
+    {
+        vertical_velocity_ = jump_speed_;
+    }
+    if (vertical_velocity_ < 0.0 && glm::abs(vertical_velocity_) > glm::abs(fall_speed_))
+    {
+        vertical_velocity_ = -glm::abs(fall_speed_);
+    }
+    vertical_offset_ = vertical_velocity_ * dt;
+
+    UpMove();
+
+    walk_direction_ = strafe_left_ + strafe_right_ + forward_ + backward_;
+
+    if (walk_direction_ != glm::vec3(0.0f))
+    {
+        walk_direction_ = glm::normalize(walk_direction_) * speed_;
+    }
+
+    WalkingMove(walk_direction_);
+
+    DownMove(dt);
+
+    // printf("%f %f %f \n", position_.x, walk_direction_.y, walk_direction_.z);
+}
+
+bool Agent::OnGround() const
+{
+    return (glm::abs(vertical_velocity_) < EPSILON && glm::abs(vertical_offset_) < EPSILON);
+}
+
+void Agent::SetLeft(const float amount)
+{
+    strafe_left_ = glm::vec3(-1.0f, 0.0f, 0.0f) * amount;
+}
+
+void Agent::SetRight(const float amount)
+{
+    strafe_right_ = glm::vec3(1.0f, 0.0f, 0.0f) * amount;
+}
+
+void Agent::SetForward(const float amount)
+{
+    forward_ = glm::vec3(0.0f, 0.0f, -1.0f) * amount;
+}
+
+void Agent::SetBackward(const float amount)
+{
+    backward_ = glm::vec3(0.0f, 0.0f, 1.0f) * amount;
+}
+
+void Agent::UnSetLeft()
+{
+    strafe_left_ = glm::vec3(0.0f);
+}
+
+void Agent::UnSetRight()
+{
+    strafe_right_ = glm::vec3(0.0f);
+}
+
+void Agent::UnSetForward()
+{
+    forward_ = glm::vec3(0.0f);
+}
+
+void Agent::UnSetBackward()
+{
+    backward_ = glm::vec3(0.0f);
+}
+
+void Agent::Jump(const glm::vec3 v)
+{
+    float len = glm::length(v);
+    jump_speed_ = len == 0 ? base_jump_speed_ : len;
+    vertical_velocity_ = jump_speed_;
+    jumping_ = true;
+
+    jump_axis_ = len == 0 ? up_ : glm::normalize(v);
+}
---
+++ src/game/fp_player.cpp
@@ -0,0 +1,114 @@
+#define GLM_FORCE_RADIANS
+#include "fp_player.h"
+
+void FP_Player::Init(GLFWwindow *w, Camera *c)
+{
+    camera_ = c;
+    c->Attach(&transform);
+    InitEventHandlers(w);
+}
+
+void FP_Player::InitEventHandlers(GLFWwindow *w)
+{
+    control_.Init(w);
+
+    control_.AddKey(
+        GLFW_KEY_W,
+        [this](float dt)
+        { this->SetForward(); },
+        GLFW_REPEAT);
+
+    control_.AddKey(
+        GLFW_KEY_S,
+        [this](float dt)
+        { this->SetBackward(); },
+        GLFW_REPEAT);
+
+    control_.AddKey(
+        GLFW_KEY_A,
+        [this](float dt)
+        { this->SetLeft(); },
+        GLFW_REPEAT);
+
+    control_.AddKey(
+        GLFW_KEY_D,
+        [this](float dt)
+        { this->SetRight(); },
+        GLFW_REPEAT);
+
+    control_.AddKey(
+        GLFW_KEY_W,
+        [this](float dt)
+        { this->UnSetForward(); },
+        GLFW_RELEASE);
+
+    control_.AddKey(
+        GLFW_KEY_S,
+        [this](float dt)
+        { this->UnSetBackward(); },
+        GLFW_RELEASE);
+
+    control_.AddKey(
+        GLFW_KEY_A,
+        [this](float dt)
+        { this->UnSetLeft(); },
+        GLFW_RELEASE);
+
+    control_.AddKey(
+        GLFW_KEY_D,
+        [this](float dt)
+        { this->UnSetRight(); },
+        GLFW_RELEASE);
+
+    control_.AddKey(
+        GLFW_KEY_SPACE,
+        [this](float dt)
+        { this->PlayerJump(); },
+        GLFW_PRESS);
+}
+
+void FP_Player::HeadMovement(float dt)
+{
+    head_bobbing_ += bobbing_speed_ * dt;
+
+    if (head_bobbing_ > 2 * glm::pi<float>())
+    {
+        head_bobbing_ = 0.0f;
+    }
+
+    float head_offset = (bobbing_amount_ * glm::sin(head_bobbing_)) * 0.5;
+
+    glm::quat bobbing_rotation = glm::angleAxis(head_offset, glm::vec3(1.0f, 0.0f, 0.0f));
+
+    glm::vec3 movement_direction = glm::normalize(transform.GetPosition() - prev_position_);
+
+    // printf("%f\n", glm::length(movement_direction));
+    if (glm::length(movement_direction) > 0.1f)
+    {
+        glm::vec3 tilt_axis = -glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), movement_direction);
+        glm::quat tilt_quaternion = glm::angleAxis(tilt_angle_, tilt_axis);
+        glm::quat target_rotation = glm::slerp(camera_->transform.GetOrientation(), tilt_quaternion, tilt_smoothing_);
+
+        camera_->transform.SetOrientation(glm::normalize(bobbing_rotation * target_rotation));
+    }
+    else
+    {
+        glm::quat target_rotation = glm::slerp(camera_->transform.GetOrientation(), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), tilt_smoothing_);
+
+        camera_->transform.SetOrientation(glm::normalize(bobbing_rotation * target_rotation));
+    }
+}
+
+void FP_Player::PlayerJump() {
+    if (OnGround()) {
+        Jump();
+    } else if (!has_dashed_) {
+        Jump(glm::normalize(glm::vec3(0.0f, 0.0f, -1.0f) + glm::vec3(0.0f, sin(dash_angle_), 0.0f)));
+        has_dashed_ = true;
+    }
+}
+
+void FP_Player::Update(float dt) {
+    Agent::Update(dt);
+    HeadMovement(dt);
+}
