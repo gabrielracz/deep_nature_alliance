faf4e7015ea3393af7bf4e91c6ccd25573bfecec
Sean Meaney <seanmeaney@protonmail.com>
2023-12-13 18:30:22 -0500

    screenspace shader in resman (maybe put it scene to make it easier when changng scenes altough scene is getting to be a fucking pig

---
+++ resources/shaders/desert_heat_vision_fp.glsl
@@ -0,0 +1,16 @@
+#version 330 core
+
+in vec2 uv_interp;
+
+out vec3 FragColor;
+
+uniform sampler2D texture_map;
+
+void main() {
+    float distanceToCenter = distance(uv_interp, vec2(0.5, 0.5));
+
+    float fade = smoothstep(0.4, 0.6, distanceToCenter);
+
+    // Interpolate between texture color and black based on the fade value
+    FragColor = mix(texture(texture_map, uv_interp).rgb, vec3(0.0), fade);
+}
