5eba458d02780527df1cde8fbc0cca029fd44fcb
Sean Meaney <seanmeaney@protonmail.com>
2023-12-16 18:06:53 -0500

    working kimda

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -887,7 +887,7 @@ void Game::SetupDesertScene() {
     scenes[DESERT]->AddTerrain(terr);
 
     auto light = std::make_shared<Light>(Colors::SunLight);
-    light->transform.SetPosition({1000.0, 1000.0, 0.0});
+    light->transform.SetPosition({300.0, 600.0, 0.0});
     light->Attach(&p->transform);
     // light->SetProjMatrix(glm::ortho(-250.0f, 250.0f, -250.0f, 250.0f, 20.0f, 1300.0f));
     scenes[DESERT]->AddLight(light);
